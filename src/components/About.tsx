98b4976911efe197f003c627327d14ab8a927a84
tapaewsky <tapaev_12@mail.ru>
2026-04-03 20:12:52 +0300

    fix: replace fake '100+ countries' stat with real '15000+ mosques'

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -117,12 +117,11 @@ export default function About() {
               }
             />
             <StatCard
-              value="100+"
-              label="Охваченных стран"
+              value="15 000+"
+              label="Мечетей в базе"
               icon={
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                  <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-                  <polyline points="9 22 9 12 15 12 15 22"/>
+                  <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                 </svg>
               }
             />
